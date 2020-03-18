4ff8590eb8df4bc0284eaec18648d68bd0d5afb0
cong258258 <1615223768@qq.com>
2020-03-19 01:02:58 +0800

    写完了 但还没有处理好exceptions

--- priority_queue/priority_queue.hpp
+++ priority_queue/priority_queue.hpp
@@ -6,66 +6,123 @@
 #include "exceptions.hpp"
 
 namespace sjtu {
-
-/**
- * a container like std::priority_queue which is a heap internal.
- */
 template<typename T, class Compare = std::less<T>>
 class priority_queue {
+private:
+	struct Node
+	{
+		Node* lc;
+		Node* rc;
+		int dist;
+		T val;
+		Node(const T &vall)
+		{
+			lc = rc = nullptr;
+			dist = 0;
+			val = vall;
+		}
+	}; 
+	Node *copy_node(const Node *other_node)
+	{
+		Node *new_node = new Node(other_node->val);
+		if(other_node->lc != nullptr)
+			new_node->lc = copy_node(other_node->lc);
+		if(other_node->rc != nullptr)
+			new_node->rc = copy_node(other_node->rc);
+		return new_node;
+	}
+	void delete_node(Node *to_delete_node)
+	{
+		if(to_delete_node == nullptr)
+			return;
+		if(to_delete_node->lc != nullptr)
+			delete_node(to_delete_node->lc);
+		if(to_delete_node->rc != nullptr)
+			delete_node(to_delete_node->rc);
+		delete to_delete_node;
+	}
+	Node *merge(Node *x, Node *y)
+	{
+		if(x == nullptr)
+			return y;
+		if(y == nullptr)
+			return x;
+		if(Compare()(x->val, y->val))
+		{
+			Node *tmp = x;
+			x = y;
+			y = tmp;
+		}
+		x->rc = merge(x->rc, y);
+		if(x->lc == nullptr || (x->rc != nullptr && x->lc->dist < x->rc->dist))
+		{
+			Node *tmp = x->lc;
+			x->lc = x->rc;
+			x->rc = tmp;
+		}
+		if(x->rc == nullptr)
+			x->dist = 0;
+		else
+			x->dist = x->rc->dist + 1;
+		return x;
+	}
+	Node *head = nullptr;
 public:
-	/**
-	 * TODO constructors
-	 */
+	size_t sizee = 0;
 	priority_queue() {}
-	priority_queue(const priority_queue &other) {}
-	/**
-	 * TODO deconstructor
-	 */
-	~priority_queue() {}
-	/**
-	 * TODO Assignment operator
-	 */
-	priority_queue &operator=(const priority_queue &other) {}
-	/**
-	 * get the top of the queue.
-	 * @return a reference of the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
-	const T & top() const {
-
+	priority_queue(const priority_queue &other)
+	{
+		head = copy_node(other.head);
+		sizee = other.sizee;
 	}
-	/**
-	 * TODO
-	 * push new element to the priority queue.
-	 */
-	void push(const T &e) {
-
+	~priority_queue()
+	{
+		delete_node(head);
 	}
-	/**
-	 * TODO
-	 * delete the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
-	void pop() {
-
+	priority_queue &operator=(const priority_queue &other)
+	{
+		delete_node(head);
+		head = copy_node(other.head);
+		sizee = other.sizee;
+		return *this;
 	}
-	/**
-	 * return the number of the elements.
-	 */
-	size_t size() const {
-
+	const T & top() const
+	{
+		if(head == nullptr)
+			throw container_is_empty();
+		return head->val;
 	}
-	/**
-	 * check if the container has at least an element.
-	 * @return true if it is empty, false if it has at least an element.
-	 */
-	bool empty() const {
-
+	void push(const T &e)
+	{
+		sizee++;
+		Node *new_node = new Node(e);
+		head = merge(head, new_node);
 	}
-	/**
-	 * return a merged priority_queue with at least O(logn) complexity.
-	 */
-	void merge(priority_queue &other) {
+	void pop()
+	{
+		if(head == nullptr)
+			throw container_is_empty();
+		else
+		{
+			Node *old_head = head;
+			head = merge(head->lc, head->rc);
+			delete old_head;
+			sizee--;
+		}
+	}
+	size_t size() const
+	{
+		return sizee;
+	}
+	bool empty() const 
+	{
+		return head == nullptr;
+	}
+	void merge(priority_queue &other)
+	{
+		sizee += other.sizee;
+		head = merge(head, other.head);
+		other.head = nullptr;
 	}
 };
 
